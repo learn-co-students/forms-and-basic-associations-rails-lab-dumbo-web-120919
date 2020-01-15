dcda9d0afce90f82f0ca7544adb62c395f50578a
hylobates-lar <alison.v.quaglia@gmail.com>
2020-01-15 18:09:09 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,18 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+    # same as:
+    # artist.nil? ? nil : artist.name
+    # reference https://coderwall.com/p/nnmjjq/try-method-do-you-need-it
+  end
 end
